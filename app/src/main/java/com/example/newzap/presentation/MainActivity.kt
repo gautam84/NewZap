bae658e9899376f738aec573beefe6f761554e0a
Gautam <gautamhazarika01@gmail.com>
2023-07-07 09:59:54 +0530

    Added Screenshots

--- app/src/main/java/com/example/newzap/presentation/MainActivity.kt
+++ app/src/main/java/com/example/newzap/presentation/MainActivity.kt
@@ -52,15 +52,3 @@ class MainActivity : ComponentActivity() {
     }
 }
 
-@Composable
-fun Greeting(name: String) {
-    Text(text = "Hello $name!")
-}
-
-@Preview(showBackground = true)
-@Composable
-fun DefaultPreview() {
-    NewZapTheme {
-        Greeting("Android")
-    }
-}
